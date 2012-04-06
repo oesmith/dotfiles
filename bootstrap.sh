1d93300f256fbe2f5817ca70bebc92b23ac3889f
Olly Smith <olly.smith@gmail.com>
2012-04-06 22:31:32 +0100

    Bootstrap script.

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -sfh ~/.dotfiles/vim ~/.vim
+ln -sf ~/.dotfiles/vimrc ~/.vimrc
+ln -sf ~/.dotfiles/gvimrc ~/.gvimrc
+ln -sf ~/.dotfiles/screenrc ~/.screenrc
+ln -sf ~/.dotfiles/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/zsh-custom/olly.zsh-theme ~/.oh-my-zsh/custom/olly.zsh-theme
