66eaca777df2683a815f9019974c67c6b716eb64
Olly Smith <ollysmith@google.com>
2013-03-01 18:11:44 +0000

    Add/update some rails aliases.

--- bash.d/6-rails-aliases.sh
+++ bash.d/6-rails-aliases.sh
@@ -1,8 +1,11 @@
 # rails shortcuts
 alias be="bundle exec"
-alias dbup="bundle exec rake db:setup && bundle exec rake db:test:prepare"
-alias dbupp="bundle exec rake db:setup && bundle exec rake db:test:prepare && bundle exec rake parallel:create && bundle exec rake parallel:prepare"
-alias rps="xvfb-run bundle exec rake parallel:spec"
+alias xbe="xvfb-run -a -s '-screen 0 1600x1200x24' bundle exec"
+alias dbup="be rake db:setup && be rake db:test:prepare"
+alias dbupc="be rake db:create && be rake db:migrate && be rake db:seed && be rake db:test:prepare"
+alias dbupp="be rake db:setup && be rake db:test:prepare && be rake parallel:create && be rake parallel:prepare"
+alias dbdn="be rake db:drop"
+alias rps="xbe rake parallel:spec"
 
 # copy example config files in $RAILS_ROOT/config/examples
 function examples() {
