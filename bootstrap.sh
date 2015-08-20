8770726f34092747c5355b7dea0bf23950f1646f
Olly Smith <ollysmith@google.com>
2015-08-20 10:48:31 +0100

    Add colordiff settings.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FILES="vim vimrc gvimrc screenrc tm_properties bashrc bash_profile \
-  tmux.conf ackrc Xdefaults scrotwm.conf"
+  tmux.conf ackrc Xdefaults scrotwm.conf colordiffrc"
 
 mklink() {
   if [[ -z "$2" ]] ; then
