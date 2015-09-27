384590052f2066d34c73d78196f55f102692bea3
Olly Smith <oliver@oesmith.co.uk>
2015-09-27 09:21:11 +0100

    Update nvm init script.

--- bash.d/5-nvm.sh
+++ bash.d/5-nvm.sh
@@ -1,1 +1,9 @@
-[[ -r ~/.nvm/nvm.sh ]] && source ~/.nvm/nvm.sh
+export NVM_DIR=$HOME/.nvm
+if [[ -d $NVM_DIR ]] ; then
+  if [[ -r ~/.nvm/nvm.sh ]] ; then
+    source ~/.nvm/nvm.sh
+  elif which brew &> /dev/null ; then
+    x=`brew --prefix nvm`/nvm.sh
+    [[ -r $x ]] && source $x
+  fi
+fi
