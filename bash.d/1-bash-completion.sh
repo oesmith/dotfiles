d2d4c8bfc1234e59007e36b7278be6d5e6ce67ec
Olly Smith <ollysmith@google.com>
2016-02-26 09:53:50 +0000

    Enable system bash completion in all terminals.

--- bash.d/1-bash-completion.sh
+++ bash.d/1-bash-completion.sh
@@ -1,3 +1,8 @@
+# Enable programmable completion features
+if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
+  . /etc/bash_completion
+fi
+
 # Homebrew-install bash-completion
 if [ -f /usr/local/etc/bash_completion ]; then
   . /usr/local/etc/bash_completion
