7eef7e3d2a8485b27496a735d630ec3d52e058fd
Olly Smith <olly.smith@gmail.com>
2020-04-27 13:41:10 +0100

    Delete some more old cruft.

--- bash.d/6-tmux.sh
+++
@@ -1,9 +0,0 @@
-# http://justinchouinard.com/blog/2010/04/10/fix-stale-ssh-environment-variables-in-gnu-screen-and-tmux/
-function r() {
-  if [[ -n $TMUX ]]; then
-    NEW_SSH_AUTH_SOCK=`find /tmp/ssh-* -name agent.* -user $USER`
-    if [[ -n $NEW_SSH_AUTH_SOCK ]] && [[ -S $NEW_SSH_AUTH_SOCK ]]; then
-      SSH_AUTH_SOCK=$NEW_SSH_AUTH_SOCK
-    fi
-  fi
-}
